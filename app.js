6dc4fd447c29e8daa5df3fd060541ca98bda528c
MATEUS LINS DE JESUS <25132834@CFP127ALUNO.LOCAL>
2025-09-02 17:00:18 -0300

    produtos

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
-'use strict'
-
-import produtos from './produtos.json' with {type: 'json'}
-
+async function carregarProdutos() {
+    try {
+      const response = await fetch("produtos.json");
+      const produtos = await response.json();
+  
+      const container = document.getElementById("produtos");
+      container.innerHTML = "";
+  
+      produtos.forEach(prod => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+  
+        // estrelas
+        let estrelas = "★".repeat(prod.classificacao) + "☆".repeat(5 - prod.classificacao);
+  
+        // preço formatado BR
+        let precoFormatado = prod.preco.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL"
+        });
+  
+        card.innerHTML = `
+          <img src="./img/${prod.imagem}" alt="${prod.nome}">
+          <h3>${prod.nome}</h3>
+          <p>${prod.descricao}</p>
+          <div class="stars">${estrelas}</div>
+          <p class="preco">${precoFormatado}</p>
+        `;
+  
+       
+        card.addEventListener("click", () => {
+          alert(`Você apertou no Produto: ${prod.nome}`);
+        });
+  
+        container.appendChild(card);
+      });
+    } catch (error) {
+      console.error("Erro ao carregar produtos:", error);
+    }
+  }
+  
+  carregarProdutos();
